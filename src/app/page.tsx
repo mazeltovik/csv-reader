c71248ca706dd5b6bdf02fe91295bac0c11a7a0c
mazeltovik <mazeltovik@gmail.com>
2024-09-22 12:33:25 +0300

    feat: add prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import RootLayout from "./layout";
+import Image from 'next/image';
+import styles from './page.module.css';
+import RootLayout from './layout';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-    </div>
-  );
+  return <div className={styles.page}></div>;
 }
