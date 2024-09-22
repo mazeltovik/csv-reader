f0c2cae94c5686c7f5afb93eba0ca09df7bcc7cd
mazeltovik <mazeltovik@gmail.com>
2024-09-22 12:24:05 +0300

    feat: add navigation component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Navigation from "@/components/ui/navigation";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,7 +27,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <Navigation/>
+        <main>{children}</main>
       </body>
     </html>
   );
